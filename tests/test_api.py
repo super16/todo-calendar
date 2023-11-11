7f74c2b7021d5e25da62e0af0b578d8e9b789859
Mark <super16mail@gmail.com>
2023-11-11 20:37:51 +0400

    Bump backend dependencies & fix trailing slash for tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,13 @@ from pytest import mark
 
 @mark.asyncio
 async def test_get_tasks_without_date_parameter(client: TestClient):
-    response = client.get("/tasks/")
+    response = client.get("/tasks")
     assert response.status_code == 422
 
 
 @mark.asyncio
 async def test_get_tasks(today_date: str, client: TestClient):
-    response = client.get("/tasks/", params={"tasks_date": today_date})
+    response = client.get("/tasks", params={"tasks_date": today_date})
     assert response.status_code == 200
 
 
@@ -21,7 +21,7 @@ async def test_create_task(
     client: TestClient,
 ):
     response = client.post(
-        "/tasks/",
+        "/tasks",
         json={
             "description": task_description,
             "tasks_date": today_date,
